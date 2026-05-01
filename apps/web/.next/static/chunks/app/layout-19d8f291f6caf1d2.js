022dbaaf73f0b3e7790805bf2cb228c3372fcf40
arpan-evita <arpansadhu13@gmail.com>
2026-05-01 19:57:25 +0530

    fix: downgrade to tailwind 3 and add configs for proper utility generation

--- apps/web/.next/static/chunks/app/layout-19d8f291f6caf1d2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{20411:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3106,23))},3106:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[727,587,855,358],()=>s(20411)),_N_E=e.O()}]);
---
+++ apps/web/.next/static/chunks/app/layout-f2e6607b42d0cffb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{20411:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3106,23))},3106:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[967,587,855,358],()=>s(20411)),_N_E=e.O()}]);
