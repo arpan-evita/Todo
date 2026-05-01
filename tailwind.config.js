c9901bf5004324790e39576e7150e1159840b0ed
arpan-evita <arpansadhu13@gmail.com>
2026-05-01 13:58:42 +0530

    Engine: Integrated Tailwind CSS for Supreme UI Accuracy v4.1

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      "colors": {
+              "primary-container": "#00f2ff",
+              "secondary-container": "#7000ff",
+              "background": "#050505",
+      },
+      "fontFamily": {
+              "display-xl": ["Inter", "sans-serif"],
+              "label-caps": ["Space Grotesk", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
