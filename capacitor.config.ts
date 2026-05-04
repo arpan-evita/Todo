82c125ab3c2917f4b63a7b4f2074078a96bb752f
arpan-evita <arpansadhu13@gmail.com>
2026-05-04 11:00:14 +0530

    feat: rename app display name to AutogrowX

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.arpan.todo',
-  appName: 'Arpan-Todo',
+  appName: 'AutogrowX',
   webDir: 'dist'
 };
 
